a722d9222cca5c785138946d905fd9f5c5f2dfeb
CT4TuEI3 <alexstatiev@gmail.com>
2024-03-20 15:45:30 +0300

    Initial commit

---
+++ VisionApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VisionApp
+//
+//  Created by CT4TuEI3 on 20.03.2024.
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
